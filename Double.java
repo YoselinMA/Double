b4b7923bf22baa10ddc68b6e5b82af3bca0551d0
YoselinMA <162406301+YoselinMA@users.noreply.github.com>
2024-05-18 22:19:03 -0600

    Create Double.java

---
+++ Double.java
@@ -0,0 +1,10 @@
+public class Double {
+    public static void main(String[] args) {
+        double num1 = 2;
+        double num2 = 38;
+        double res = 0;
+        res = num1/num2;
+        System.out.println("El resultado de dividir " +num1 +"/"+ num2+" = "+res);
+    }
+    
+}
